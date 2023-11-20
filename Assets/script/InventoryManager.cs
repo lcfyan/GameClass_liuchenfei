ec3566f045bc059124b492d54e56499ab1316546
lcfyan <lcfyan@outlook.com>
2023-11-21 00:22:05 +0800

    背包系统
    背包系统

---
+++ Assets/script/InventoryManager.cs
@@ -0,0 +1,56 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class InventoryManager : MonoBehaviour
+{
+    static InventoryManager instance;
+
+    public Inventory myBag;
+    public GameObject slotGrid;
+    public slot slotprefab;
+    public Text itemInfo;
+
+    void Awake()
+    {
+        if (instance != null)
+            Destroy(this);
+        instance = this;
+    }
+
+    private void OnEnable()
+    {
+        RefreshItem();
+        instance.itemInfo.text = "";
+    }
+
+    public static void UpdateItemInfo(string itemDescription)
+    {
+        instance.itemInfo.text = itemDescription;
+    }
+
+    public static void CreateNewItem(Item item)
+    {
+        slot newItem = Instantiate(instance.slotprefab, instance.slotGrid.transform.position, Quaternion.identity);
+        newItem.gameObject.transform.SetParent(instance.slotGrid.transform);
+        newItem.slotItem = item;
+        newItem.slotImage.sprite = item.itemImage;
+        newItem.slotNum.text = item.itemHeld.ToString();
+    }
+
+    public static void RefreshItem()
+    {
+        for (int i = 0; i < instance.slotGrid.transform.childCount; i++)
+        {
+            if (instance.slotGrid.transform.childCount == 0)
+                break;
+            Destroy(instance.slotGrid.transform.GetChild(i).gameObject);
+        }
+
+        for (int i = 0; i < instance.myBag.itemList.Count; i++)
+        {
+            CreateNewItem(instance.myBag.itemList[i]);
+        }
+    }
+}
--- Assets/script/Move.cs
+++ Assets/script/Move.cs
@@ -7,16 +7,29 @@ public class Move : MonoBehaviour
     private CharacterController cc;
     [Header("移动参数")]
     public float moveSpeed;
-
     private float horizontalMove, verticalMove;
     private Vector3 dir;
+
+    public GameObject myBag;
+    bool isOpen;
 
     void Start()
     {
         cc = GetComponent<CharacterController>();
     }
 
+    void Update()
+    {
+        MoveCharacter();
+        OpenMyBag();
+    }
+
     void FixedUpdate()
+    {
+        // Use FixedUpdate for character movement physics if necessary
+    }
+
+    void MoveCharacter()
     {
         horizontalMove = Input.GetAxis("Horizontal") * moveSpeed;
         verticalMove = Input.GetAxis("Vertical") * moveSpeed;
@@ -24,5 +37,13 @@ public class Move : MonoBehaviour
         dir = transform.forward * verticalMove + transform.right * horizontalMove;
         cc.Move(dir * Time.deltaTime);
     }
-}
 
+    void OpenMyBag()
+    {
+        if (Input.GetKeyDown(KeyCode.B))
+        {
+            isOpen = !isOpen;
+            myBag.SetActive(isOpen);
+        }
+    }
+}
